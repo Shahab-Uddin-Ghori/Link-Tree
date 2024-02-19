2ea6d6cd893b9aa0c4771356fdc0ccd68dd3d4e2
Shahab Uddin Ghori <shahabnisar918@gmail.com>
2024-02-19 15:26:45 +0500

    trying img as bg 2

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
   }, false);
   
+  /*==================== typed js ====================*/
+
+const typed = new Typed(".myname", {
+    strings: [
+      "gh pr status",
+      " gh pr checkout",
+      "gh pr create",
+      " gh pr checks",
+      "gh alias set",
+    ],
+    typeSpeed: 100,
+    backSpeed: 0,
+    backDelay: 1000,
+    loop: true,
+    cursorChar: "",
+  });
